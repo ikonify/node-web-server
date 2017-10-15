2d95b3128b5e1200695793392e93631e3bf54e37
Ikonify <hemisalo@gmail.com>
2017-10-15 11:48:52 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log ('Unable to write to server log.');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+// maintenance mode
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+       pageTitle: 'Website Root',
+       welcomeMessage: 'Welcome to the Root Website' 
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error getting data'
+    });
+});
+
+var port = process.env.port || 3000;
+app.listen(3000, () => {
+    console.log(`Server is running on port ${port}`);
+});
